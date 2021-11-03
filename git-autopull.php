7c3935e832591f76c902b8a33f7640808f9f7c4c
Noah Emke <noahremke@gmail.com>
2021-11-03 18:44:49 -0400

    Removed verification on autopull for troubleshooting

--- git-autopull.php
+++ git-autopull.php
@@ -3,16 +3,16 @@
 
 if ($_POST) {
     // Check the GitHub secret
-    if (hash_equals('sha256=' +
-            hash_hmac(
-            'sha256', 
-            $_POST, $_ENV['HTTP_GITHUB_SECRET']), 
-        $_SERVER['HTTP__X_HUB_SIGNATURE_256'])) {
+    //if (hash_equals('sha256=' +
+    //        hash_hmac(
+    //        'sha256', 
+    //        $_POST, $_ENV['HTTP_GITHUB_SECRET']), 
+    //    $_SERVER['HTTP__X_HUB_SIGNATURE_256'])) {
 
         shell_exec( 'cd /var/www/html/ && git pull origin main')
-    }else {
-        header("HTTP/1.1 403 Forbidden");
-    }
+    //}else {
+    //    header("HTTP/1.1 403 Forbidden");
+    //}
 }else {
         header("HTTP/1.1 400 Bad Request");
     }
